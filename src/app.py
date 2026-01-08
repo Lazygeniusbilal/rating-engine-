ea51a7c2d04ae6b1824acad852e476fa8c5ca513
Lazygeniusbilal <bilal.ahmed38980@gmail.com>
2026-01-08 10:36:32 -1200

    added cors

--- src/app.py
+++ src/app.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.rating_engine.routes.api import router as api_router
 from src.rating_engine.routes.health import router as health_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials= True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(api_router)
 app.include_router(health_router)
